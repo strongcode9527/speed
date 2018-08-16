e667b15de7f14fe20711cd9fc56f2af3308ed4e0
李壮 <1477869317@qq.com>
2018-08-16 16:34:07 +0800

    :sparkles:

--- src/render/render.js
+++ src/render/render.js
@@ -1,11 +1,10 @@
 import {pathOr, path} from 'ramda'
 
 import update from './updateComponent'
-import EFFECTS from '../structure/effects'
+import {dispatchLifeCycle} from '../utils'
 import renderFactory from './renderFactory'
 import tags from '../structure/componentTags'
 import {createFiber} from '../structure/fiber'
-import {createArrayChild, dispatchLifeCycle} from '../utils'
 
 export function render(element, root) {
   // 将根节点放入渲染列表中。
@@ -30,57 +29,13 @@ function workLoop(deadline) {
     
     renderFactory.nextUnitOfWork = createUnitOfWork(renderFactory.nextUnitOfWork)
     if(renderFactory.pendingCommit) {
+      console.log(renderFactory.pendingCommit)
       commitWork(renderFactory.pendingCommit)
     }
   }
 }
 
-/**
- * 为所有子节点Vnode创建fiber，并且建立联系，并为currenFiber绑定child
- * @param {Vnode} childs 
- * @param {Fiber} currentFiber 
- */
-function handleChildrenVnode(childs, currentFiber) {
 
-  let prevFiber = null, oldChild = currentFiber.child
-  childs.forEach((child, index) => {
-    
-    // child 是vnode，而不是fiber
-    let newFiber = null
-    if(typeof child === 'object') {
-      newFiber = createFiber(undefined, child.type, undefined, child.props, currentFiber)
-    }else {
-      newFiber = createFiber(tags.HostText, null, undefined, {children: [child]}, currentFiber)
-    }
-
-    const isSame = oldChild && newFiber && child.type === newFiber.type
-
-    // 实例化节点,update处理多样化子节点。
-    update(newFiber)
-    // 更新
-    if(isSame) {
-
-    }
-    // 添加
-    else if(!isSame && newFiber) {
-      newFiber.effectTag = EFFECTS.PLACEMENT
-    }
-    // 修改
-    else {
-
-    }
-
-    prevFiber && (prevFiber.sibing = newFiber)
-
-    prevFiber = newFiber
-
-    
-    index === 0 && (currentFiber.child = newFiber)
-
-    oldChild = path(['sibling'], oldChild)
-    
-  })
-}
 
 
 /**
@@ -90,20 +45,13 @@ function handleChildrenVnode(childs, currentFiber) {
  */
 function createUnitOfWork(currentFiber) {
 
-  let childs = []
-  if(path(['type', 'prototype', 'isClassComponent'], currentFiber) || path(['stateNode', 'constructor', 'prototype', 'isClassComponent'], currentFiber)) {
-    childs = createArrayChild(currentFiber.stateNode.render())
-  }
-  else if(typeof path(['type'], currentFiber) === 'function') {
-    childs = createArrayChild(currentFiber.stateNode(currentFiber.props))
-  }else {
-    childs = createArrayChild(pathOr([], ['props', 'children'], currentFiber))
-  }
-  console.log(currentFiber)
   if(currentFiber.child) return currentFiber.child
 
+  update(currentFiber)
+
+  
   // 意味着这个currentFiber已经是叶子节点了，只能返回上一层寻找兄弟节点。
-  if(!currentFiber.type && !path(['stateNode', '__relative'],currentFiber)) {
+  if(!currentFiber.type) {
     while(currentFiber) {
       if(currentFiber.tag === tags.HostRoot) {
         renderFactory.pendingCommit = currentFiber
@@ -119,14 +67,10 @@ function createUnitOfWork(currentFiber) {
       currentFiber = currentFiber.return
     }
   }
-  
-  
-  // console.log(currentFiber)
 
-  handleChildrenVnode(childs, currentFiber)
- 
   
-
+  
+  // console.log(currentFiber) 
   return currentFiber.child
 }
 
@@ -159,7 +103,9 @@ function commitWork(fiber) {
     while(parent.tag === tags.ClassComponent || parent.tag === tags.FunctionalComponent) {
       parent = parent.return
     }
-    
+
+    console.log(effect.stateNode, parent.stateNode);
+
     (effect.tag !== tags.ClassComponent && effect.tag !== tags.FunctionalComponent) && parent.stateNode.appendChild(effect.stateNode)
     if(effect.tag === tags.ClassComponent) {
       dispatchLifeCycle(effect.stateNode, 'componentDidMount')
--- src/render/updateComponent.js
+++ src/render/updateComponent.js
@@ -1,56 +1,131 @@
-import {path} from 'ramda'
+import {path, pathOr} from 'ramda'
 
+import {createArrayChild} from '../utils'
+import EFFECTS from '../structure/effects'
 import tags from '../structure/componentTags'
+import {createFiber} from '../structure/fiber'
+
 
 // 更新和创建在一起。
 export default function update(fiber) {
 
   if(path(['type', 'prototype', 'isClassComponent'], fiber)) {
-    return upgradeClassComponent(fiber)
+    return updateClassComponent(fiber)
   }
 
   else if(typeof fiber.type === 'function') {
-    return upgradeFunctionComponent(fiber)
+    return updateFunctionComponent(fiber)
   }
 
   else if(typeof fiber.type === 'string'){
-    return upgradeDomComponent(fiber)
+    return updateDomComponent(fiber)
   }
   
   else {
-    return upgradeTextComponent(fiber)
+    return updateTextComponent(fiber)
   }
 }
 
 
-function upgradeClassComponent(fiber) {
-  // 更新
-  if(fiber.stateNode) {
-    console.log('in upgrade')
-  }
-  // 创建
-  else {
+function updateClassComponent(fiber) {
+  // 创建fiber
+  if(!fiber.stateNode) {
     const instantiation = new fiber.type(fiber.props)
     fiber.tag = tags.ClassComponent
     fiber.stateNode = instantiation
     fiber.stateNode.__relative = fiber
-    return fiber
   }
+
+  fiber.stateNode.state = {...fiber.stateNode.state, ...fiber.partialState}
   
+  return handleChildrenVnode(fiber.stateNode.__relative, createChilds(fiber))
 }
 
-function upgradeFunctionComponent(fiber) {
+function updateFunctionComponent(fiber) {
   const children = fiber.type(fiber.props)
   fiber.tag = tags.FunctionalComponent
   fiber.stateNode = children
 }
 
-function upgradeDomComponent(fiber) {
-  const dom = document.createElement(fiber.type)
-  fiber.tag = tags.HostComponent
-  fiber.stateNode = dom
+function updateDomComponent(fiber) {
+  if(!fiber.stateNode) {
+    const dom = document.createElement(fiber.type)
+    fiber.tag = fiber.tag || tags.HostComponent
+    fiber.stateNode = dom
+  }
+  return handleChildrenVnode(fiber, createChilds(fiber))
 }
 
-function upgradeTextComponent(fiber) {
+function updateTextComponent(fiber) {
   fiber.stateNode = document.createTextNode(fiber.props.children[0])
 }
+
+/**
+ * 为所有子节点Vnode创建fiber，并且建立联系，并为currenFiber绑定child
+ * @param {Fiber} currentFiber 
+ * @param {Vnode} childs 
+ */
+function handleChildrenVnode(currentFiber, childs) {
+  let oldChildFiber = currentFiber.child
+
+  let prevFiber = null
+
+  childs.forEach((child, index) => {
+    
+    // child 是vnode，而不是fiber
+    let newFiber = null
+    if(typeof child === 'object') {
+      newFiber = createFiber(undefined, child.type, undefined, child.props, currentFiber)
+    }else {
+      newFiber = createFiber(tags.HostText, null, undefined, {children: [child]}, currentFiber)
+    }
+
+    const isSame = oldChildFiber && newFiber && oldChildFiber.type === newFiber.type
+
+    // 实例化节点,update处理多样化子节点。
+    update(newFiber)
+    // 更新
+    if(isSame) {
+      newFiber.effectTag = EFFECTS.UPDATE
+    }
+    // 添加
+    else if(!isSame && newFiber && !oldChildFiber) {
+      newFiber.effectTag = EFFECTS.PLACEMENT
+    }
+
+    // 删除
+    else if(!isSame && oldChildFiber){
+      
+    }
+
+    prevFiber && (prevFiber.sibing = newFiber)
+
+    prevFiber = newFiber
+
+    
+    index === 0 && (currentFiber.child = newFiber)
+
+    oldChildFiber = path(['sibling'], oldChildFiber)
+    
+  })
+  return currentFiber
+}
+
+
+
+function createChilds(currentFiber) {
+  let childs = []
+  if(path(['type', 'prototype', 'isClassComponent'], currentFiber) || path(['stateNode', 'constructor', 'prototype', 'isClassComponent'], currentFiber)) {
+    childs = createArrayChild(currentFiber.stateNode.render())
+  }
+  else if(typeof path(['type'], currentFiber) === 'function') {
+    childs = createArrayChild(currentFiber.stateNode(currentFiber.props))
+  }else {
+    childs = createArrayChild(pathOr([], ['props', 'children'], currentFiber))
+  }
+  return childs
+}
+
+
+
+
