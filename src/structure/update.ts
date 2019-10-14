9bd33090d7dd7c5f93f04b6f92e06864bcbc8085
QITMAC000948 <1477869317@qq.com>
2019-10-14 10:31:09 +0800

    :sparkles:

--- src/structure/update.ts
+++ src/structure/update.ts
@@ -1,19 +1,28 @@
 import batchingStrategy from './batchingStrategy';
 
+// setState入口
 export default {
   enqueueUpdate(Component): void {
     
-    // 直接更新
-    if (!batchingStrategy.isBatchingUpdates) {
-      batchingStrategy.batchedUpdates(enqueueUpdate, component);
-      return;
-    }
+    // // 直接更新
+    // if (!batchingStrategy.isBatchingUpdates) {
+    //   batchingStrategy.batchedUpdates(enqueueUpdate, component);
+    //   return;
+    // }
 
-    /// 队列更新。
-    dirtyComponents.push(component);
-    if (component._updateBatchNumber == null) {
-      component._updateBatchNumber = updateBatchNumber + 1;
-    }
+    // /// 队列更新。
+    // dirtyComponents.push(component);
+    // if (component._updateBatchNumber == null) {
+    //   component._updateBatchNumber = updateBatchNumber + 1;
+    // }
+
+
+
+    // 首先创建更新的权重
+    const update = createUpdate();
+    
+    // 其次直接进行更新
+    
   }
 };
 
