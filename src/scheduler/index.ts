add4dc342712a351fff8e8aa43631d8a95f3fa0c
strong <1477869317@qq.com>
2020-02-18 15:38:46 +0800

    :sparkles:

--- src/scheduler/index.ts
+++ src/scheduler/index.ts
@@ -7,6 +7,7 @@ let currentPriority = NORMAL_PRIORITY;
 
 // 任务链表，从前往后过期时间越来越低。
 let firstTask: Task, lastTask: Task;
+let isRunningTasks = false;
 
 interface Task {
   next: Task;
@@ -22,6 +23,97 @@ function createTask(callback: Function, expirationTime: number): Task {
   };
 }
 
+
+let rAFId: number;
+let timeoutId;
+
+function requestAnimationFrameWithTimeout(callback: Function): any {
+  if(isRunningTasks) {
+    return;
+  }
+  isRunningTasks = true;
+  rAFId = requestAnimationFrame(function(timesTamp: number): void {
+    callback(timesTamp);
+    clearTimeout(timeoutId);
+  });
+
+  timeoutId = setTimeout(function(): void {
+    cancelAnimationFrame(rAFId);
+    callback(performance.now());
+  }, 100);
+}
+
+// 去调用保存的任务
+function flushTasks(): void {
+  if(isRunningTasks) {
+    return;
+  }
+  try {
+    firstTask.callback();
+    if(firstTask === lastTask) {
+      firstTask = undefined;
+      lastTask = undefined;
+    } else {
+      firstTask = firstTask.next;
+    }
+  } finally {
+    isRunningTasks = false;
+  }
+}
+
+// We use the postMessage trick to defer idle work until after the repaint.
+const channel = new MessageChannel();
+const port = channel.port2;
+
+
+let frameDeadline = 0;
+// We start out assuming that we run at 30fps but then the heuristic tracking
+// will adjust this value to a faster fps if we get more frequent animation
+// frames.
+let previousFrameTime = 33;
+let activeFrameTime = 33;
+
+// requestAnimationFrame 的回调函数
+function animationTick (rafTime: number): void {
+  let nextFrameTime = rafTime - frameDeadline + activeFrameTime;
+  if (nextFrameTime < activeFrameTime && previousFrameTime < activeFrameTime) {
+    if (nextFrameTime < 8) {
+      // Defensive coding. We don't support higher frame rates than 120hz.
+      // If the calculated frame time gets lower than 8, it is probably a bug.
+      nextFrameTime = 8;
+    }
+    // If one frame goes long, then the next one can be short to catch up.
+    // If two frames are short in a row, then that's an indication that we
+    // actually have a higher frame rate than what we're currently optimizing.
+    // We adjust our heuristic dynamically accordingly. For example, if we're
+    // running on 120hz display or 90hz VR display.
+    // Take the max of the two in case one of them was an anomaly due to
+    // missed frame deadlines.
+    activeFrameTime = nextFrameTime < previousFrameTime ? previousFrameTime : nextFrameTime;
+  } else {
+    previousFrameTime = nextFrameTime;
+  }
+  
+  frameDeadline = rafTime + activeFrameTime;
+  port.postMessage(undefined);
+};
+
+channel.port1.onmessage = function (): void {
+  const currentTime = performance.now();
+  if (frameDeadline < currentTime) {
+    // 现在已经没有时间了
+    requestAnimationFrameWithTimeout(animationTick);
+  } else {
+    // 还有剩余时间
+    flushTasks();
+  }
+};
+
+/**
+ * 设定任务
+ * @param callback 回调函数
+ * @param expirationTime 过期时间
+ */
 export function setTaskWithExpirationTime(callback: Function, expirationTime: number): void {
   const newTask: Task = createTask(callback, expirationTime);
   // 之前没有任务，现在插入
@@ -52,9 +144,11 @@ export function setTaskWithExpirationTime(callback: Function, expirationTime: nu
         currentTask = currentTask.next;
       } while(currentTask !== firstTask );
     }
-
   }
+  
 }
+
+
 
 
 
